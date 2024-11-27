6b52d5ba7fc3b7d651c563ef6c0ee665913a379b
Steve Todorov <steve-todorov@users.noreply.github.com>
2024-11-27 22:42:02 +0200

    Update secret.sh

--- secret.sh
+++ secret.sh
@@ -4,4 +4,11 @@
 PASSWORD="bazing"
 echo "::add-mask::${PASSWORD}"
 
+OUTPUT_PREFIX=$1
+
+cat <<EOF
+${OUTPUT_PREFIX}_jdbc=example1
+${OUTPUT_PREFIX}_database=example2
+EOF
+
 echo "top_secret=${PASSWORD}" >> "$GITHUB_OUTPUT"
